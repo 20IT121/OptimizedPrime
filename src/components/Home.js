a7c3b54f9a13eaf25446eff6d0edcdfa122989dc
Dharmendra <20IT117@charusat.edu.in>
2022-08-31 14:05:35 +0530

    Audio added in button on click

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,12 @@
-import React from 'react'
-import '../css/home.css'
+import React from 'react';
+import '../css/home.css';
+import sound from "../voices/positive2.wav";
 
 export default function Home() {
-  // var audio = new Audio('collectingmaterial.wav');
-  // const buttons = document.querySelectorAll(".button1");
-  // buttons.forEach(button => {
-  //   button.addEventListener("click", () => {
-  //     audio.play();
-  //   });
-  // });
-  // const voice = () => {
-  //   audio.play();
-  // }
+
+  function play(){
+    new Audio(sound).play();
+  }
 
   return (
     <div className='container-fluid'>
@@ -24,7 +19,7 @@ export default function Home() {
           <p className="card-text">A data structure is a specialized format for organizing, processing, retrieving and storing data. It is a way of storing data in a computer so that it can be used efficiently and it will allow
             the most efficient algorithm to be used. The choice of the data structure begins from the choice of an
             abstract data type (ADT).</p>
-          <a href="/" className="btn btn-primary button-home d-grid gap-2 col-4 mx-auto">CLICK HERE</a>
+          <button className="btn btn-primary button-home d-grid gap-2 col-4 mx-auto" onClick={play}>CLICK HERE</button>
         </div>
       </div>
 
@@ -34,7 +29,7 @@ export default function Home() {
         </div>
         <div className="card-body">
           <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa cupiditate beatae nihil, reprehenderit perspiciatis hic suscipit itaque voluptates, qui voluptatem est doloremque veritatis quisquam molestiae quam ex adipisci? Deleniti, beatae! Fugiat, natus. Pariatur perspiciatis unde voluptatibus consequuntur delectus. Commodi vitae earum cupiditate aperiam hic ea fuga corporis veniam, assumenda eius! Inventore quis illo totam soluta assumenda?</p>
-          <a href="/" className="btn btn-primary button-home d-grid gap-2 col-4 mx-auto">CLICK HERE</a>
+          <button className="btn btn-primary button-home d-grid gap-2 col-4 mx-auto" onClick={play}>CLICK HERE</button>
         </div>
       </div>
 
@@ -46,7 +41,7 @@ export default function Home() {
           <p className="card-text">A data structure is a specialized format for organizing, processing, retrieving and storing data. It is a way of storing data in a computer so that it can be used efficiently and it will allow
             the most efficient algorithm to be used. The choice of the data structure begins from the choice of an
             abstract data type (ADT).</p>
-          <a href="/" className="btn btn-primary button-home d-grid gap-2 col-4 mx-auto">CLICK HERE</a>
+          <button className="btn btn-primary button-home d-grid gap-2 col-4 mx-auto" onClick={play}>CLICK HERE</button>
         </div>
       </div>
 
